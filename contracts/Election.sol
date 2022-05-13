42e5e5e610c56767063313db12e3a47acbc764c9
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-13 14:58:00 +0530

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Election  {
+    // Store Candidate
+    string public candidate;
+
+    // Constructor
+    constructor() {
+        candidate = "Candidate1";
+    }
+    
+    // Read Candidate
+}
