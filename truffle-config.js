6150b080901d4c6b154b3fac6ead07ecdfdacefd
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-19 13:24:50 +0530

    Initial commit for refactoring client to react

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,22 @@
+const path = require("path");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+    develop: {
+      port: 8545
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
-    }
   },
   compilers: {
     solc: {
-      version: ">=0.6.0 <0.9.0",
+      version: ">=0.4.0 <0.9.0",
       optimizer: {
         enabled: true,
         runs: 200
