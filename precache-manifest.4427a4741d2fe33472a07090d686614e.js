b65963f0c66f7e484bdd3b9d11c5ccfe167b8ae4
myusername <harshal4269@gmail.com>
2022-07-03 16:30:09 +0530

    Updates

---
+++ precache-manifest.4427a4741d2fe33472a07090d686614e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2c7e96c04d6b7cf356c1c53a882fc2",
+    "url": "/Voting-App-Solidity/index.html"
+  },
+  {
+    "revision": "8b877875d0995f35ae90",
+    "url": "/Voting-App-Solidity/static/css/2.5f10bdda.chunk.css"
+  },
+  {
+    "revision": "f7d279034977f7a0d891",
+    "url": "/Voting-App-Solidity/static/css/main.4ca1eb6d.chunk.css"
+  },
+  {
+    "revision": "8b877875d0995f35ae90",
+    "url": "/Voting-App-Solidity/static/js/2.f84bfe3f.chunk.js"
+  },
+  {
+    "revision": "f7d279034977f7a0d891",
+    "url": "/Voting-App-Solidity/static/js/main.95880bf4.chunk.js"
+  },
+  {
+    "revision": "0ea980f207df25b9f821",
+    "url": "/Voting-App-Solidity/static/js/runtime-main.0d2131b0.js"
+  }
+]);
